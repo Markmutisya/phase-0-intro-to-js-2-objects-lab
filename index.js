4fa001bad32d82eac1994cf9baedacfe9ed45ad2
Markmutisya <mark.mwangi@dtudent.moringaschool.com>
2022-06-09 13:06:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+name : 'Mutisya' ,
+streetaddress : '124 Kiambu'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+    const newEmployee = {...employee};
+    newEmployee[key]=value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue( employee, key, value)
+{
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key)
+{
+    const deleteEmployee = {...employee};
+    delete deleteEmployee[key];
+    return deleteEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+    delete employee[key];
+    return employee;
+}
